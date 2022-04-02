271625f1c0b6c5ffda85b321cd9b11a8dbbf7879
luccaloopz <luccamartins2012@gmail.com>
2022-04-02 13:40:00 -0700

    added a highscores form where you add your initals to the high scores list. Also, the hyperlink on the top-left corner of the page takes the user to the highscores list. From the highscores list users can either go back to the main page or clear all of their highscores if they so choose.

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,6 +1,11 @@
 var startButton = document.querySelector("#start");
 var welcomeContainer = document.querySelector("#welcome-container");
 var questionContainer = document.querySelector("#question-container");
+var highscores = document.querySelector("#highscores");
+var highscoresForm = document.querySelector("#highscores-form");
+var highscoresList = document.querySelector("#highscores-list");
+var goBack = document.querySelector("#go-back");
+var clearHighscores = document.querySelector("#clear-highscores")
 
 startButton.addEventListener("click", startTheGame);
 
@@ -8,4 +13,28 @@ function startTheGame() {
     console.log("The game has started");
     welcomeContainer.setAttribute("class", "hide");
     questionContainer.setAttribute("class", "visible");
+}
+
+highscores.addEventListener("click", showHighscores);
+
+function showHighscores() {
+    welcomeContainer.setAttribute("class", "hide");
+    questionContainer.setAttribute("class", "hide");
+    highscoresForm.setAttribute("class", "hide"); //may not need this im not sure yet...
+    highscoresList.setAttribute("class", "visible");
+}
+
+goBack.addEventListener("click", goBackToMainPage);
+
+function goBackToMainPage() {
+    welcomeContainer.setAttribute("class", "visible");
+    questionContainer.setAttribute("class", "hide");
+    highscoresForm.setAttribute("class", "hide"); //may not need this im not sure yet...
+    highscoresList.setAttribute("class", "hide");
+}
+
+clearHighscores.addEventListener("click", clearAllTheHighscores);
+
+function clearAllTheHighscores() {
+    console.log("highscores are cleared");
 }
