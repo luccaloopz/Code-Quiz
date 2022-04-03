8b2e0f97c30ce1df232ed47453b1ea86dadf1a71
luccaloopz <luccamartins2012@gmail.com>
2022-04-02 18:41:44 -0700

    made changes to the question bank array in order to make it easier to identify a correct answer

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -6,6 +6,12 @@ var highscoresForm = document.querySelector("#highscores-form");
 var highscoresList = document.querySelector("#highscores-list");
 var goBack = document.querySelector("#go-back");
 var clearHighscores = document.querySelector("#clear-highscores");
+var answerText = document.querySelector(".answersABCD");
+var displayedQuestion = document.querySelector("#question");
+var nextButton = document.querySelector("#next");
+var button = document.querySelector(".button")
+
+var currentQuestion;
 
 startButton.addEventListener("click", startTheGame);
 
@@ -13,6 +19,8 @@ function startTheGame() {
     console.log("The game has started");
     welcomeContainer.setAttribute("class", "hide");
     questionContainer.setAttribute("class", "visible");
+    currentQuestion = 0;
+    nextQuestionUp();
 }
 
 highscores.addEventListener("click", showHighscores);
@@ -31,67 +39,77 @@ function goBackToMainPage() {
     questionContainer.setAttribute("class", "hide");
     highscoresForm.setAttribute("class", "hide"); //may not need this im not sure yet...
     highscoresList.setAttribute("class", "hide");
+
 }
 
 clearHighscores.addEventListener("click", clearAllTheHighscores);
 
 function clearAllTheHighscores() {
     console.log("highscores are cleared");
+    //this will need to clear highscores
 }
 
 function nextQuestionUp() {
-
+    grabQuestion(questions[currentQuestion]); //grabs the question bank array at the current index (initially set to 0)
+    
 }
+
+function grabQuestion(question) {
+    // this will run a for loop to show the current question and available answers to the user
+    displayedQuestion.textContent = question.question;
+    question.answers.forEach(element => {
+        answerText.textContent = element.written
+        if (element.correctness) {
+            button.dataset.correctness = element.correctness;
+        }
+    });
+}
+
 
 const questions = [
     {
         question: "What does HTML stand for?",
-        answer: {
-            a: "HyperText Markup Language",
-            b: "Hyper Mark Language",
-            c: "Higher Markup Language",
-            d: "None of the above",
-        },
-        correctAnswer: "a"
+        answers: [
+            {written: "HyperText Markup Language", correctness: true},
+            {written: "Hyper Mark Language", correctness: false},
+            {written: "Higher Markup Language", correctness: false},
+            {written: "None of the above", correctness: false}
+        ],
     },
     {
         question: "What does CSS stand for?",
-        answer: {
-            a: "Column Style Sheets",
-            b: "Cascading Style Sheets",
-            c: "Capture Style Sheets",
-            d: "None of the above",
-        },
-        correctAnswer: "b"
+        answers: [
+            {written: "Column Style Sheets", correctness: false},
+            {written: "Cascading Style Sheets", correctness: true},
+            {written: "Capture Style Sheets", correctness: false},
+            {written: "None of the above", correctness: false},
+        ],
     },
     {
         question: "Which of the following HTML tags creates a line-break on the page?",
-        answer: {
-            a: "<br>",
-            b: "<img>",
-            c: "<a>",
-            d: "<p>",
-        },
-        correctAnswer: "a"
+        answers: [
+            {written: "<br>", correctness: true},
+            {written: "<img>", correctness: false},
+            {written: "<a>", correctness: false},
+            {written: "<p>", correctness: false},
+        ],
     },
     {
         question: "Which of the following is not a valid JavaScript variable name?",
-        answer: {
-            a: "_2names",
-            b: "_first_and_last_names",
-            c: "FirstAndLast",
-            d: "None of the above",
-        },
-        correctAnswer: "a"
+        answers: [
+            {written: "_2names", correctness: true},
+            {written: "_first_and_last_names", correctness: false},
+            {written: "FirstAndLast", correctness: false},
+            {written: "None of the above", correctness: false},
+        ],
     },
     {
         question: "Inside which of the following HTML elements do we put/link the JavaScript?",
-        answer: {
-            a: "<js>",
-            b: "<scripting>",
-            c: "<script>",
-            d: "<javascript>",
-        },
-        correctAnswer: "c"
+        answers: [
+            {written: "<js>", correctness: false},
+            {written: "<scripting>", correctness: false},
+            {written: "<script>", correctness: true},
+            {written: "<javascript>", correctness: false},
+        ],
     },
 ]
