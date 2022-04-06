48f05e4eb418638632f670832b3260b094564f06
luccaloopz <luccamartins2012@gmail.com>
2022-04-05 18:45:14 -0700

    if user does not finish quiz in time, game is over and score is not logged.

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -9,10 +9,12 @@ var clearHighscores = document.querySelector("#clear-highscores");
 var answerBtns = document.querySelector(".answers");
 var displayedQuestion = document.querySelector("#question");
 var timer = document.querySelector(".timer");
+var score = document.querySelector("#score");
+var initials = document.querySelector("#initials");
 
 var currentQuestion;
 
-var secondsLeft = 120;
+var secondsLeft = 5;
 
 startButton.addEventListener("click", startTheGame);
 
@@ -22,113 +24,92 @@ function startTheGame() {
     currentQuestion = 0;
     nextQuestionUp();
     setTime();
-}
+};
 
-highscores.addEventListener("click", showHighscores); // **this will need to be a button instead
+highscores.addEventListener("click", showHighscores);
 
 function showHighscores() {
     welcomeContainer.setAttribute("class", "hide");
     questionContainer.setAttribute("class", "hide");
-    highscoresForm.setAttribute("class", "hide"); //may not need this im not sure yet...
+    highscoresForm.setAttribute("class", "hide");
     highscoresList.setAttribute("class", "visible");
-}
+};
 
 goBack.addEventListener("click", goBackToMainPage);
 
 function goBackToMainPage() {
     welcomeContainer.setAttribute("class", "visible");
     questionContainer.setAttribute("class", "hide");
-    highscoresForm.setAttribute("class", "hide"); //may not need this im not sure yet...
+    highscoresForm.setAttribute("class", "hide");
     highscoresList.setAttribute("class", "hide");
 
-}
+};
 
 clearHighscores.addEventListener("click", clearAllTheHighscores);
 
 function clearAllTheHighscores() {
     console.log("highscores are cleared");
     //**this will need to clear highscores
-}
+};
 
 function nextQuestionUp() {
-    // for (var i = 0; i < questions.length; i++) {
-    //     grabQuestion(questions[i]);
-    //     console.log("test");
-    // }
-    // grabQuestion(questions[0]); //grabs the question bank array at the current index (initially set to 0)
-    // if (document.querySelector("button").clicked === true) {
-    //     console.log("test")
-    // }
-    resetQuestions()
+    resetQuestions();
     if (currentQuestion < questions.length) {
         grabQuestion(questions[currentQuestion])
     } else {
-        highscoresForm.setAttribute("class", "visible")
+        score.textContent = secondsLeft;
+        window.localStorage.setItem("myScore", secondsLeft.toString()); //not sure if correct
+        
+        highscoresForm.setAttribute("class", "visible");
         questionContainer.setAttribute("class", "hide");
-    }
-}
+    };
+};
 
 function grabQuestion(question) {
-    // this will run a for loop to show the current question and available answers to the user 
     displayedQuestion.textContent = question.question;
     question.answers.forEach(element => {
-        var button = document.createElement("button")
-        button.innerText = element.written
-        button.setAttribute("class", "button")
+        var button = document.createElement("button");
+        button.innerText = element.written;
+        button.setAttribute("class", "button");
         if (element.correctness) {
             button.dataset.correctness = element.correctness;
-        }
-        answerBtns.appendChild(button)
+        };
+        answerBtns.appendChild(button);
         button.addEventListener("click", function(){
             evaluateAnswer(element.correctness);
         });
-    }); 
-
-    //** If user answers question correctly, they are moved on to a new problem. If they answer wrong, timer gets deducted and they move onto new problem */
-}
+    });
+};
 
 function resetQuestions() {
     while (answerBtns.firstChild) {
         answerBtns.removeChild(answerBtns.firstChild)
-    }
-}
- 
-// button.addEventListener("click", chosenAnswer)
-// function chosenAnswer(event) {
-// //     //if chosen answer is wrong, decrease timer by 20 seconds(secondsLeft var?. in other words 'secondsLeft=secondsLeft -20'?) 
-// //     //* We might only need to log the ending time to localStorage so that highscores can pull the data */
-//     var chosenAnswer = event.target
-//     var correct = chosenAnswer.dataset.correctness
-//     if (correct) {
-//         currentQuestion++
-//         grabQuestion()
-//     } else {
-//         secondsLeft = secondsLeft -20
-//     }
-// }
+    };
+};
 
 function setTime() {
     var timerInterval = setInterval(function() {
-        secondsLeft--
-        timer.textContent = "Seconds left: "  + secondsLeft
+        secondsLeft--;
+        timer.textContent = "Seconds left: "  + secondsLeft;
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
             welcomeContainer.setAttribute("class", "visible");
+            highscoresForm.setAttribute("class", "hide");
             questionContainer.setAttribute("class", "hide");
             return secondsLeft;
         }
     }, 1000)
-}
+};
 
 function evaluateAnswer(correctness) {
-    console.log(correctness)
-    currentQuestion++
+    console.log(correctness);
+    currentQuestion++;
     if (!correctness) {
-        secondsLeft -= 20
+        secondsLeft -= 20;
     }
-    nextQuestionUp()
-}
+    nextQuestionUp();
+};
 
 const questions = [
     {
@@ -176,4 +157,4 @@ const questions = [
             {written: "<javascript>", correctness: false},
         ],
     },
-]
+];
